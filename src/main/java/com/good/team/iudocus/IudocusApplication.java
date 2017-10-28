978c16226a1fcf7e767ae5b7761a17b9c9f468e0
Wojtek Pietrucha <wojtekpietrucha@gmail.com>
2017-10-28 20:45:53 +0200

    initial project

---
+++ src/main/java/com/good/team/iudocus/IudocusApplication.java
@@ -0,0 +1,12 @@
+package com.good.team.iudocus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IudocusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IudocusApplication.class, args);
+	}
+}
